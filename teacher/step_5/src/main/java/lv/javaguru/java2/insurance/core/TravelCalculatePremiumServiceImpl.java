34ed9fb4faa1f9e17715ade9f754d431489416bd
javagurulv <javagurulv@gmail.com>
2023-03-09 15:08:35 +0200

    step 5

---
+++ teacher/step_5/src/main/java/lv/javaguru/java2/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -0,0 +1,31 @@
+package lv.javaguru.java2.insurance.core;
+
+import lv.javaguru.java2.insurance.rest.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.rest.TravelCalculatePremiumResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
+
+    @Autowired private DateTimeService dateTimeService;
+
+    @Override
+    public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        response.setPersonFirstName(request.getPersonFirstName());
+        response.setPersonLastName(request.getPersonLastName());
+        response.setAgreementDateFrom(request.getAgreementDateFrom());
+        response.setAgreementDateTo(request.getAgreementDateTo());
+
+        var daysBetween = dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo());
+        response.setAgreementPrice(new BigDecimal(daysBetween));
+
+        return response;
+    }
+
+}
