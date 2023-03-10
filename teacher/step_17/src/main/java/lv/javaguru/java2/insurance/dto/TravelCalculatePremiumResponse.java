86ff9c7a8091923d7f1e183634504b0cb5525a44
javagurulv <javagurulv@gmail.com>
2023-03-10 13:56:49 +0200

    step 17

--- teacher/step_17/src/main/java/lv/javaguru/java2/insurance/dto/TravelCalculatePremiumResponse.java
+++ teacher/step_17/src/main/java/lv/javaguru/java2/insurance/dto/TravelCalculatePremiumResponse.java
@@ -6,13 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TravelCalculatePremiumRequest {
+public class TravelCalculatePremiumResponse extends CoreResponse {
 
     private String personFirstName;
     private String personLastName;
@@ -22,5 +24,11 @@ public class TravelCalculatePremiumRequest {
 
     @JsonFormat(pattern="yyyy-MM-dd")
     private Date agreementDateTo;
+
+    private BigDecimal agreementPrice;
+
+    public TravelCalculatePremiumResponse(List<ValidationError> errors) {
+        super(errors);
+    }
 
 }
