556645a54eecd6d9940f222be9e6968bad67fa7e
javagurulv <javagurulv@gmail.com>
2023-03-10 09:02:54 +0200

    step 12

---
+++ teacher/step_12/src/main/java/lv/javaguru/java2/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -0,0 +1,43 @@
+package lv.javaguru.java2.insurance.core;
+
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumResponse;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Component
+class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
+
+    @Autowired private TravelCalculatePremiumRequestValidator validator;
+    @Autowired private DateTimeService dateTimeService;
+
+    @Override
+    public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        List<ValidationError> errors = validator.validate(request);
+        return errors.isEmpty()
+                ? buildResponse(request)
+                : buildResponse(errors);
+    }
+
+    private TravelCalculatePremiumResponse buildResponse(List<ValidationError> errors) {
+        return new TravelCalculatePremiumResponse(errors);
+    }
+
+    private TravelCalculatePremiumResponse buildResponse(TravelCalculatePremiumRequest request) {
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        response.setPersonFirstName(request.getPersonFirstName());
+        response.setPersonLastName(request.getPersonLastName());
+        response.setAgreementDateFrom(request.getAgreementDateFrom());
+        response.setAgreementDateTo(request.getAgreementDateTo());
+
+        var daysBetween = dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo());
+        response.setAgreementPrice(new BigDecimal(daysBetween));
+
+        return response;
+    }
+
+}
