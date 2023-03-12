7920a482f3efdff62a392d967d5d65af1b6ed66f
javagurulv <javagurulv@gmail.com>
2023-03-12 15:57:10 +0200

    step 21

---
+++ teacher/step_21/src/main/java/lv/javaguru/java2/insurance/core/validations/AgreementDateFromInFutureValidation.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java2.insurance.core.validations;
+
+import lv.javaguru.java2.insurance.core.DateTimeService;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+class AgreementDateFromInFutureValidation implements TravelRequestValidation {
+
+    @Autowired private DateTimeService dateTimeService;
+
+    @Override
+    public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
+        Date dateFrom = request.getAgreementDateFrom();
+        Date currentDateTime = dateTimeService.getCurrentDateTime();
+        return (dateFrom != null && dateFrom.before(currentDateTime))
+                ? Optional.of(new ValidationError("agreementDateFrom", "Must be in the future!"))
+                : Optional.empty();
+    }
+
+}
