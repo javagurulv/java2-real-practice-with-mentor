c688401fd11d577bda52c65beab50ee94815ba99
javagurulv <javagurulv@gmail.com>
2023-03-12 19:57:36 +0200

    step 22

---
+++ teacher/step_22/src/main/java/lv/javaguru/java2/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -0,0 +1,41 @@
+package lv.javaguru.java2.insurance.core;
+
+import lv.javaguru.java2.insurance.core.validations.TravelCalculatePremiumRequestValidator;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumResponse;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Component
+class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
+
+    @Autowired private TravelCalculatePremiumRequestValidator validator;
+    @Autowired private TravelPremiumUnderwriting premiumUnderwriting;
+
+    @Override
+    public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        List<ValidationError> errors = validator.validate(request);
+        return errors.isEmpty()
+                ? buildResponse(request, premiumUnderwriting.calculatePremium(request))
+                : buildResponse(errors);
+    }
+
+    private TravelCalculatePremiumResponse buildResponse(List<ValidationError> errors) {
+        return new TravelCalculatePremiumResponse(errors);
+    }
+
+    private TravelCalculatePremiumResponse buildResponse(TravelCalculatePremiumRequest request, BigDecimal premium) {
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        response.setPersonFirstName(request.getPersonFirstName());
+        response.setPersonLastName(request.getPersonLastName());
+        response.setAgreementDateFrom(request.getAgreementDateFrom());
+        response.setAgreementDateTo(request.getAgreementDateTo());
+        response.setAgreementPrice(premium);
+        return response;
+    }
+
+}
