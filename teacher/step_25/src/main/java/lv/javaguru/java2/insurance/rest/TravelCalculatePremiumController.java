f9ae7992e7416906dc356edb387089f8a5f11f4d
javagurulv <javagurulv@gmail.com>
2023-03-13 08:17:34 +0200

    step 26

--- teacher/step_25/src/main/java/lv/javaguru/java2/insurance/rest/TravelCalculatePremiumController.java
+++ teacher/step_25/src/main/java/lv/javaguru/java2/insurance/rest/TravelCalculatePremiumController.java
@@ -1,11 +1,8 @@
 package lv.javaguru.java2.insurance.rest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lv.javaguru.java2.insurance.core.TravelCalculatePremiumService;
 import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- teacher/step_26/src/main/java/lv/javaguru/java2/insurance/rest/TravelCalculatePremiumController.java
+++ teacher/step_26/src/main/java/lv/javaguru/java2/insurance/rest/TravelCalculatePremiumController.java
@@ -1,5 +1,6 @@
 package lv.javaguru.java2.insurance.rest;
 
+import com.google.common.base.Stopwatch;
 import lv.javaguru.java2.insurance.core.TravelCalculatePremiumService;
 import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumResponse;
@@ -11,14 +12,25 @@ import org.springframework.web.bind.annotation.*;
 public class TravelCalculatePremiumController {
 
 	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
+	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
+	@Autowired private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
 	@Autowired private TravelCalculatePremiumService calculatePremiumService;
 
 	@PostMapping(path = "/",
 			consumes = "application/json",
 			produces = "application/json")
 	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		TravelCalculatePremiumResponse response = processRequest(request);
+		executionTimeLogger.logExecutionTime(stopwatch);
+		return response;
+	}
+
+	private TravelCalculatePremiumResponse processRequest(TravelCalculatePremiumRequest request) {
 		requestLogger.log(request);
-		return calculatePremiumService.calculatePremium(request);
+		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+		responseLogger.log(response);
+		return response;
 	}
 
 }
