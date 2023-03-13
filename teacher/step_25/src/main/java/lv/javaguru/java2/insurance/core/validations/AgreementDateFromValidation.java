5c4322e49c4e05cdd8d3c888017e5b4de8680450
javagurulv <javagurulv@gmail.com>
2023-03-13 07:55:58 +0200

    step 25

---
+++ teacher/step_25/src/main/java/lv/javaguru/java2/insurance/core/validations/AgreementDateFromValidation.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java2.insurance.core.validations;
+
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+class AgreementDateFromValidation implements TravelRequestValidation {
+
+    @Override
+    public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
+        return (request.getAgreementDateFrom() == null)
+                ? Optional.of(new ValidationError("agreementDateFrom", "Must not be empty!"))
+                : Optional.empty();
+    }
+
+}
---
+++ teacher/step_25/src/main/java/lv/javaguru/java2/insurance/rest/TravelCalculatePremiumController.java
@@ -0,0 +1,30 @@
+package lv.javaguru.java2.insurance.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lv.javaguru.java2.insurance.core.TravelCalculatePremiumService;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/insurance/travel")
+public class TravelCalculatePremiumController {
+
+	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
+	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
+	@Autowired private TravelCalculatePremiumService calculatePremiumService;
+
+	@PostMapping(path = "/",
+			consumes = "application/json",
+			produces = "application/json")
+	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+		requestLogger.log(request);
+		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+		responseLogger.log(response);
+		return response;
+	}
+
+}
