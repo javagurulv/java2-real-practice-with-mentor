952d9d47a63a68811a83b20eb3700250da3ddd6d
javagurulv <javagurulv@gmail.com>
2023-03-20 12:00:23 +0200

    step 44

---
+++ teacher/step_44/src/main/java/lv/javaguru/java2/insurance/core/validations/DateFromLessThenDateToValidation.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java2.insurance.core.validations;
+
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+class DateFromLessThenDateToValidation implements TravelRequestValidation {
+
+    @Autowired private ValidationErrorFactory errorFactory;
+
+    @Override
+    public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
+        Date dateFrom = request.getAgreementDateFrom();
+        Date dateTo = request.getAgreementDateTo();
+        return (dateFrom != null && dateTo != null
+                && (dateFrom.equals(dateTo) || dateFrom.after(dateTo)))
+                ? Optional.of(errorFactory.buildError("ERROR_CODE_5"))
+                : Optional.empty();
+    }
+
+}
