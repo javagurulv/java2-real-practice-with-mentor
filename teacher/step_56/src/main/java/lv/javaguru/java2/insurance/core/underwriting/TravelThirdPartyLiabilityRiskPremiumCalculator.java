613866a9c5d80c97c24f0fd34d343f1b8da6f7c9
javagurulv <javagurulv@gmail.com>
2023-03-22 10:22:03 +0200

    step 56

---
+++ teacher/step_56/src/main/java/lv/javaguru/java2/insurance/core/underwriting/TravelThirdPartyLiabilityRiskPremiumCalculator.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java2.insurance.core.underwriting;
+
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+class TravelThirdPartyLiabilityRiskPremiumCalculator implements TravelRiskPremiumCalculator {
+
+    @Override
+    public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
+        return BigDecimal.ZERO;
+    }
+
+    @Override
+    public String getRiskIc() {
+        return "TRAVEL_THIRD_PARTY_LIABILITY";
+    }
+
+}
