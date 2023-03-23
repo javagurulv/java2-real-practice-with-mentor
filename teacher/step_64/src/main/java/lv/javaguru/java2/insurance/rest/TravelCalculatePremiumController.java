614b88b3208b249d9453cd45ef96377909c69dab
javagurulv <javagurulv@gmail.com>
2023-03-23 16:15:19 +0200

    step 64

---
+++ teacher/step_64/src/main/java/lv/javaguru/java2/insurance/rest/TravelCalculatePremiumController.java
@@ -0,0 +1,36 @@
+package lv.javaguru.java2.insurance.rest;
+
+import com.google.common.base.Stopwatch;
+import lv.javaguru.java2.insurance.core.services.TravelCalculatePremiumService;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/insurance/travel")
+public class TravelCalculatePremiumController {
+
+	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
+	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
+	@Autowired private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
+	@Autowired private TravelCalculatePremiumService calculatePremiumService;
+
+	@PostMapping(path = "/",
+			consumes = "application/json",
+			produces = "application/json")
+	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		TravelCalculatePremiumResponse response = processRequest(request);
+		executionTimeLogger.logExecutionTime(stopwatch);
+		return response;
+	}
+
+	private TravelCalculatePremiumResponse processRequest(TravelCalculatePremiumRequest request) {
+		requestLogger.log(request);
+		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+		responseLogger.log(response);
+		return response;
+	}
+
+}
