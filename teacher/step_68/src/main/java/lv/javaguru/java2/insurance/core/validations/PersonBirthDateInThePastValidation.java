0ef87e26c7079d8f9953c35d1c84460eab00edb1
javagurulv <javagurulv@gmail.com>
2023-03-25 11:53:28 +0200

    step 68

---
+++ teacher/step_68/src/main/java/lv/javaguru/java2/insurance/core/validations/PersonBirthDateInThePastValidation.java
@@ -0,0 +1,27 @@
+package lv.javaguru.java2.insurance.core.validations;
+
+import lv.javaguru.java2.insurance.core.util.DateTimeUtil;
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+class PersonBirthDateInThePastValidation extends TravelRequestValidationImpl {
+
+    @Autowired private DateTimeUtil dateTimeUtil;
+    @Autowired private ValidationErrorFactory errorFactory;
+
+    @Override
+    public Optional<ValidationError> validate(TravelCalculatePremiumRequest request) {
+        Date personBirthDate = request.getPersonBirthDate();
+        Date currentDateTime = dateTimeUtil.getCurrentDateTime();
+        return (personBirthDate != null && personBirthDate.after(currentDateTime))
+                ? Optional.of(errorFactory.buildError("ERROR_CODE_12"))
+                : Optional.empty();
+    }
+
+}
