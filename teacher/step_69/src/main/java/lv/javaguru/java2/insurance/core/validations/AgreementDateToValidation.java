a167e51eb20eb8a8f3650d4d8a13de25f63f7823
javagurulv <javagurulv@gmail.com>
2023-03-26 10:04:27 +0300

    step 69

---
+++ teacher/step_69/src/main/java/lv/javaguru/java2/insurance/core/validations/AgreementDateToValidation.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java2.insurance.core.validations;
+
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+class AgreementDateToValidation extends TravelRequestValidationImpl {
+
+    @Autowired private ValidationErrorFactory errorFactory;
+
+    @Override
+    public Optional<ValidationError> validate(TravelCalculatePremiumRequest request) {
+        return (request.getAgreementDateTo() == null)
+                ? Optional.of(errorFactory.buildError("ERROR_CODE_4"))
+                : Optional.empty();
+    }
+
+}
