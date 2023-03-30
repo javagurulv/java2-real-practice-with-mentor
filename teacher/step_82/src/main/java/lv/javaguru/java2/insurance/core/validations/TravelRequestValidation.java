fffeed525600f4aff2c66c93616a99c880efe87a
javagurulv <javagurulv@gmail.com>
2023-03-30 12:51:24 +0300

    step 82

---
+++ teacher/step_82/src/main/java/lv/javaguru/java2/insurance/core/validations/TravelRequestValidation.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java2.insurance.core.validations;
+
+import lv.javaguru.java2.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.java2.insurance.dto.ValidationError;
+
+import java.util.List;
+import java.util.Optional;
+
+interface TravelRequestValidation {
+
+    Optional<ValidationError> validate(TravelCalculatePremiumRequest request);
+
+    List<ValidationError> validateList(TravelCalculatePremiumRequest request);
+
+}
