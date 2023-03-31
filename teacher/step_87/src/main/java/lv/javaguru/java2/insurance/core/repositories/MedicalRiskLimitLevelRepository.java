fa5c783d3303bee2533bc79fc7121c511acd9122
javagurulv <javagurulv@gmail.com>
2023-03-31 12:30:48 +0300

    step 87

---
+++ teacher/step_87/src/main/java/lv/javaguru/java2/insurance/core/repositories/MedicalRiskLimitLevelRepository.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java2.insurance.core.repositories;
+
+import lv.javaguru.java2.insurance.core.domain.MedicalRiskLimitLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MedicalRiskLimitLevelRepository
+        extends JpaRepository<MedicalRiskLimitLevel, Long> {
+
+    Optional<MedicalRiskLimitLevel> findByMedicalRiskLimitLevelIc(String medicalRiskLimitLevelIc);
+
+}
