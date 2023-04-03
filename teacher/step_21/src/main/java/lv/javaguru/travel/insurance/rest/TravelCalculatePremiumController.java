20a80b06c977ad6b1a9addf949e866d985c939a8
javagurulv <javagurulv@gmail.com>
2023-04-03 11:00:12 +0300

    step 30

---
+++ teacher/step_21/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -0,0 +1,22 @@
+package lv.javaguru.travel.insurance.rest;
+
+import lv.javaguru.travel.insurance.core.TravelCalculatePremiumService;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/insurance/travel")
+public class TravelCalculatePremiumController {
+
+	@Autowired private TravelCalculatePremiumService calculatePremiumService;
+
+	@PostMapping(path = "/",
+			consumes = "application/json",
+			produces = "application/json")
+	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+		return calculatePremiumService.calculatePremium(request);
+	}
+
+}
