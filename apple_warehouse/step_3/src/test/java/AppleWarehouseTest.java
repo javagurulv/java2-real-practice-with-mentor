fd9f16c3eef30e22aa64a1487b5422132b636542
javagurulv <javagurulv@gmail.com>
2023-08-09 10:02:26 +0300

    apple warehouse

---
+++ apple_warehouse/step_3/src/test/java/AppleWarehouseTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppleWarehouseTest {
+
+    private AppleWarehouse warehouse = new AppleWarehouse();
+
+    @Test
+    public void shouldFindGreenApples() {
+        List<Apple> greenApples = warehouse.findGreenApples();
+        assertEquals(greenApples.size(), 3);
+    }
+
+    @Test
+    public void shouldFindRedApples() {
+        List<Apple> greenApples = warehouse.findRedApples();
+        assertEquals(greenApples.size(), 3);
+    }
+
+}
