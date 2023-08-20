cd067459cab12186779b7d975111195283df2093
javagurulv <javagurulv@gmail.com>
2023-08-20 15:24:04 +0300

    bank transactions step 29

---
+++ bank_transactions/step_29/src/main/java/org/javaguru/banktransactions/MaxBalanceCalculatorImpl.java
@@ -0,0 +1,17 @@
+package org.javaguru.banktransactions;
+
+class MaxBalanceCalculatorImpl implements MaxBalanceCalculator {
+    @Override
+    public int calculate(BankAccount bankAccount) {
+        int maxBalance = 0;
+        CurrentBalanceHolder currentBalanceHolder = new CurrentBalanceHolder();
+        for (Transaction transaction : bankAccount.getTransactions()) {
+            int currentBalance = currentBalanceHolder.applyTransaction(transaction);
+            if (currentBalance > maxBalance) {
+                maxBalance = currentBalance;
+            }
+        }
+        return maxBalance;
+    }
+
+}
