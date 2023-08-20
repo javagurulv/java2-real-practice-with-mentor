82f006d16d12801808f76db854b96a5cd6ecfa09
javagurulv <javagurulv@gmail.com>
2023-08-20 10:00:39 +0300

    bank transactions step 21

---
+++ bank_transactions/step_22/src/main/java/org/javaguru/banktransactions/MaxBalanceCalculatorForImpl.java
@@ -0,0 +1,23 @@
+package org.javaguru.banktransactions;
+
+class MaxBalanceCalculatorForImpl implements MaxBalanceCalculator {
+    @Override
+    public int calculate(BankAccount bankAccount) {
+        int maxBalance = 0;
+        int currentBalance = 0;
+        Transaction[] transactions = bankAccount.getTransactions();
+        for (int i = 0; i < transactions.length; i++) {
+            Transaction transaction = transactions[i];
+            if (transaction.isDeposit()) {
+                currentBalance = currentBalance + transaction.amount();
+            }
+            if (transaction.isWithdrawal()) {
+                currentBalance = currentBalance - transaction.amount();
+            }
+            if (currentBalance > maxBalance) {
+                maxBalance = currentBalance;
+            }
+        }
+        return maxBalance;
+    }
+}
---
+++ bank_transactions/step_22/src/test/java/org/javaguru/banktransactions/MaxBalanceCalculatorImplTest.java
@@ -0,0 +1,52 @@
+package org.javaguru.banktransactions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxBalanceCalculatorImplTest {
+
+    private MaxBalanceCalculator calculator = new MaxBalanceCalculatorImpl();
+
+    @Test
+    public void shouldReturnZeroForBankAccountWithoutTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin", new Transaction[]{});
+        assertEquals(0, calculator.calculate(bankAccount));
+    }
+
+    @Test
+    public void shouldReturnMaxForDepositTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin",
+                new Transaction[]{
+                        ImmutableTransaction.of(100, TransactionType.DEPOSIT),
+                        ImmutableTransaction.of(100, TransactionType.DEPOSIT)
+                }
+        );
+        assertEquals(200, calculator.calculate(bankAccount));
+    }
+
+    @Test
+    public void shouldReturnMaxForWithdrawalTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin",
+                new Transaction[]{
+                        ImmutableTransaction.of(100, TransactionType.WITHDRAWAL),
+                        ImmutableTransaction.of(100, TransactionType.WITHDRAWAL)
+                }
+        );
+        assertEquals(0, calculator.calculate(bankAccount));
+    }
+
+    @Test
+    public void shouldReturnMaxForDepositAndWithdrawalTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin",
+                new Transaction[]{
+                        ImmutableTransaction.of(100, TransactionType.WITHDRAWAL),
+                        ImmutableTransaction.of(100, TransactionType.DEPOSIT),
+                        ImmutableTransaction.of(50, TransactionType.DEPOSIT),
+                        ImmutableTransaction.of(50, TransactionType.WITHDRAWAL)
+                }
+        );
+        assertEquals(50, calculator.calculate(bankAccount));
+    }
+
+}
