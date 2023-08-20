856c16df668b246d46553888ae4a23fd9c7641a0
javagurulv <javagurulv@gmail.com>
2023-08-20 10:09:27 +0300

    bank transactions step 23

---
+++ bank_transactions/step_23/src/test/java/org/javaguru/banktransactions/CurrentBalanceCalculatorStreamApiImplTest.java
@@ -0,0 +1,53 @@
+package org.javaguru.banktransactions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrentBalanceCalculatorStreamApiImplTest {
+
+    private CurrentBalanceCalculator calculator = new CurrentBalanceCalculatorStreamApiImpl();
+
+    @Test
+    public void shouldReturnZeroForBankAccountWithoutTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin", new Transaction[]{});
+        assertEquals(0, calculator.calculate(bankAccount));
+    }
+
+    @Test
+    public void shouldReturnSumDepositTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin",
+                new Transaction[]{
+                        ImmutableTransaction.of(100, TransactionType.DEPOSIT),
+                        ImmutableTransaction.of(100, TransactionType.DEPOSIT)
+                }
+        );
+        assertEquals(200, calculator.calculate(bankAccount));
+    }
+
+    @Test
+    public void shouldReturnSumWithdrawalTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin",
+                new Transaction[]{
+                        ImmutableTransaction.of(100, TransactionType.WITHDRAWAL),
+                        ImmutableTransaction.of(100, TransactionType.WITHDRAWAL)
+                }
+        );
+        assertEquals(-200, calculator.calculate(bankAccount));
+    }
+
+    @Test
+    public void shouldReturnSumDepositAndWithdrawalTransactions() {
+        BankAccount bankAccount = new BankAccount("Vasja Pupkin",
+                new Transaction[]{
+                        ImmutableTransaction.of(100, TransactionType.WITHDRAWAL),
+                        ImmutableTransaction.of(100, TransactionType.DEPOSIT),
+                        ImmutableTransaction.of(50, TransactionType.DEPOSIT),
+                        ImmutableTransaction.of(50, TransactionType.WITHDRAWAL)
+                }
+        );
+        assertEquals(0, calculator.calculate(bankAccount));
+    }
+
+
+}
