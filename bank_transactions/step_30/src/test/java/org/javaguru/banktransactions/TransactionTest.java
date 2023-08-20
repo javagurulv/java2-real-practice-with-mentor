5dfb75ccbf6bd313a3a906d79f3f619db03f1520
javagurulv <javagurulv@gmail.com>
2023-08-20 15:36:31 +0300

    bank transactions step 30

---
+++ bank_transactions/step_30/src/test/java/org/javaguru/banktransactions/TransactionTest.java
@@ -0,0 +1,30 @@
+package org.javaguru.banktransactions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionTest {
+
+    @Test
+    public void shouldCreateBook() {
+        Transaction transaction = ImmutableTransaction.of(
+                100,
+                TransactionType.DEPOSIT);
+        assertNotNull(transaction);
+        assertEquals(transaction.amount(), 100);
+        assertEquals(transaction.transactionType(), TransactionType.DEPOSIT);
+    }
+
+    @Test
+    public void shouldBuildTransactionWithBuilder() {
+        Transaction transaction = ImmutableTransaction.builder()
+                .amount(100)
+                .transactionType(TransactionType.DEPOSIT)
+                .build();
+        assertNotNull(transaction);
+        assertEquals(transaction.amount(), 100);
+        assertEquals(transaction.transactionType(), TransactionType.DEPOSIT);
+    }
+
+}
