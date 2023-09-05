3c52544ed610dc18ba6fe7f0f0ca85901e153d31
javagurulv <javagurulv@gmail.com>
2023-09-05 09:34:47 +0300

    shape - 9

---
+++ geometry_shape/step_9/src/main/java/org/javaguru/geometry/shapes/Shape.java
@@ -0,0 +1,14 @@
+package org.javaguru.geometry.shapes;
+
+abstract class Shape {
+
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+    abstract double calculateArea();
+    abstract double calculatePerimeter();
+
+}
