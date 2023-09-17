7f51dd19e58a4f7e724b41603709e34e8b548135
javagurulv <javagurulv@gmail.com>
2023-09-17 11:42:11 +0300

    lesson 1 solutions

---
+++ java_software_engineer/lesson_2_variables/src/main/java/org/javaguru/mentor/lesson_2/homework/solutions/day_2/task_1/LoginService.java
@@ -0,0 +1,9 @@
+package org.javaguru.mentor.lesson_2.homework.solutions.day_2.task_1;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+}
