173045bd3e2876229d5a6e8091ad6d828d47357f
javagurulv <javagurulv@gmail.com>
2023-09-17 23:06:49 +0300

    lesson 3 solutions

---
+++ java_software_engineer/lesson_3_oop_first_look/src/main/java/org/javaguru/mentor/lesson_3/homework/solutions/level_5_middle/task_1/Book.java
@@ -0,0 +1,15 @@
+package org.javaguru.mentor.lesson_3.homework.solutions.level_5_middle.task_1;
+
+class Book {
+
+    private String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
