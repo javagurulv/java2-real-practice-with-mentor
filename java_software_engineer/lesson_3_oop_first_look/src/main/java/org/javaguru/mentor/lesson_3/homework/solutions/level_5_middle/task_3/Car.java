ba2ed73e2adec707cfec0f2ce610d6a06bdbf774
javagurulv <javagurulv@gmail.com>
2023-09-17 23:14:09 +0300

    lesson 3 solutions

---
+++ java_software_engineer/lesson_3_oop_first_look/src/main/java/org/javaguru/mentor/lesson_3/homework/solutions/level_5_middle/task_3/Car.java
@@ -0,0 +1,15 @@
+package org.javaguru.mentor.lesson_3.homework.solutions.level_5_middle.task_3;
+
+class Car {
+
+    private String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
