1e1f42d044f6436d5d39043c9f24e0411f00ddd5
javagurulv <javagurulv@gmail.com>
2023-09-17 16:51:26 +0300

    lesson 3 solutions

---
+++ java_software_engineer/lesson_3_oop_first_look/src/main/java/org/javaguru/mentor/lesson_3/homework/solutions/level_1_intern/task_4/Robot.java
@@ -0,0 +1,18 @@
+package org.javaguru.mentor.lesson_3.homework.solutions.level_1_intern.task_4;
+
+class Robot {
+
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
