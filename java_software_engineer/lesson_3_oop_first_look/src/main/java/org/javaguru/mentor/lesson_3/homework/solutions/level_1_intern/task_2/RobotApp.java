8c39edf03a49f1e893882f97e0ef0587ed722efd
javagurulv <javagurulv@gmail.com>
2023-09-17 16:47:47 +0300

    lesson 3 solutions

---
+++ java_software_engineer/lesson_3_oop_first_look/src/main/java/org/javaguru/mentor/lesson_3/homework/solutions/level_1_intern/task_2/RobotApp.java
@@ -0,0 +1,10 @@
+package org.javaguru.mentor.lesson_3.homework.solutions.level_1_intern.task_2;
+
+class RobotApp {
+
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("Vasja");
+        Robot robot2 = new Robot("Petja");
+    }
+
+}
