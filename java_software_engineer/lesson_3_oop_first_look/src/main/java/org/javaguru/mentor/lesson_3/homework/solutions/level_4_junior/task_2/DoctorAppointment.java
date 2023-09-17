04d601e85a29f8d121dc7a28888ed5154d67bcbb
javagurulv <javagurulv@gmail.com>
2023-09-17 18:51:37 +0300

    lesson 3 solutions

---
+++ java_software_engineer/lesson_3_oop_first_look/src/main/java/org/javaguru/mentor/lesson_3/homework/solutions/level_4_junior/task_2/DoctorAppointment.java
@@ -0,0 +1,20 @@
+package org.javaguru.mentor.lesson_3.homework.solutions.level_4_junior.task_2;
+
+class DoctorAppointment {
+
+    private String patientFirstName;
+    private String patientLastName;
+    private String diagnosis;
+    private int visitPrice;
+
+    public DoctorAppointment(String patientFirstName,
+                             String patientLastName,
+                             String diagnosis,
+                             int visitPrice) {
+        this.patientFirstName = patientFirstName;
+        this.patientLastName = patientLastName;
+        this.diagnosis = diagnosis;
+        this.visitPrice = visitPrice;
+    }
+
+}
