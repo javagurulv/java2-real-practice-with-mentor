14a5b1ff83f5f911e8e152fd4f08770ee83d721c
javagurulv <javagurulv@gmail.com>
2023-09-17 18:10:20 +0300

    lesson 3 solutions

---
+++ java_software_engineer/lesson_3_oop_first_look/src/main/java/org/javaguru/mentor/lesson_3/homework/solutions/level_3_junior/task_2/Chapter.java
@@ -0,0 +1,13 @@
+package org.javaguru.mentor.lesson_3.homework.solutions.level_3_junior.task_2;
+
+class Chapter {
+
+    private String title;
+    private String text;
+
+    public Chapter(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+}
