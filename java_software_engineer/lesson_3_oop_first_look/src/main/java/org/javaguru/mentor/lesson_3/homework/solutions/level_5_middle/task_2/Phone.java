6f8420a7e6edad013e60a745385b1646bf0aa5c7
javagurulv <javagurulv@gmail.com>
2023-09-17 23:12:22 +0300

    lesson 3 solutions

---
+++ java_software_engineer/lesson_3_oop_first_look/src/main/java/org/javaguru/mentor/lesson_3/homework/solutions/level_5_middle/task_2/Phone.java
@@ -0,0 +1,15 @@
+package org.javaguru.mentor.lesson_3.homework.solutions.level_5_middle.task_2;
+
+class Phone {
+
+    private String model;
+
+    Phone(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
