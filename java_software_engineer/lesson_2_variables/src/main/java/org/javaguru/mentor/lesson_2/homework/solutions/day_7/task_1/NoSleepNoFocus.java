6e43d5279dcb95652454edcd1943c12cbebe9663
javagurulv <javagurulv@gmail.com>
2023-09-17 16:21:32 +0300

    lesson 1 solutions

---
+++ java_software_engineer/lesson_2_variables/src/main/java/org/javaguru/mentor/lesson_2/homework/solutions/day_7/task_1/NoSleepNoFocus.java
@@ -0,0 +1,10 @@
+package org.javaguru.mentor.lesson_2.homework.solutions.day_7.task_1;
+
+class NoSleepNoFocus {
+
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+
+}
