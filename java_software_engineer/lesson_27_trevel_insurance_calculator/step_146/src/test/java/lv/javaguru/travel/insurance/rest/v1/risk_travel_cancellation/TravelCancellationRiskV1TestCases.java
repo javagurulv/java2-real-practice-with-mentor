368662b48f530a21d7b152355349c92e2b939779
javagurulv <javagurulv@gmail.com>
2023-11-14 17:12:03 +0200

    step 146

---
+++ java_software_engineer/lesson_27_trevel_insurance_calculator/step_146/src/test/java/lv/javaguru/travel/insurance/rest/v1/risk_travel_cancellation/TravelCancellationRiskV1TestCases.java
@@ -0,0 +1,11 @@
+package lv.javaguru.travel.insurance.rest.v1.risk_travel_cancellation;
+
+import lv.javaguru.travel.insurance.rest.v1.TravelCalculatePremiumControllerV1TestCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TravelCancellationRiskV1TestCases extends TravelCalculatePremiumControllerV1TestCase {
+
+
+
+}
