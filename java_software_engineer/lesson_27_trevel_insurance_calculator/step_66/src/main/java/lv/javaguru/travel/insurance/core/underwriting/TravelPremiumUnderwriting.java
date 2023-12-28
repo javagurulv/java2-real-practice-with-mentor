0ee2a6ccdb3f80855743ee0b2bfb7c7514999147
javagurulv <javagurulv@gmail.com>
2023-12-28 10:02:44 +0200

    rename package lv -> org step 61 - 80

--- java_software_engineer/lesson_27_trevel_insurance_calculator/step_66/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwriting.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.travel.insurance.core.underwriting;
-
-import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
-
-public interface TravelPremiumUnderwriting {
-
-    TravelPremiumCalculationResult calculatePremium(TravelCalculatePremiumRequest request);
-
-}
